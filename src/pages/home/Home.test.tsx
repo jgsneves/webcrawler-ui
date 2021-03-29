e6991430abbc172cbf116c3f14982e8f10b2a36a
joao gabriel neves <jgsn.adv@gmail.com>
2021-03-28 22:04:11 -0300

    home test

--- src/pages/home/Home.test.tsx
+++ src/pages/home/Home.test.tsx
@@ -3,8 +3,8 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import App from '../../App';
 
 describe("home page test", () => {
-    render(<App />);
     it("should render entire home page", () => {
+        render(<App />);
         const title = screen.getByText("Webcrawler");
         const subTitle = screen.getByText("Automação na busca de termos na internet");
         const explanationText = screen.getByText(
@@ -18,11 +18,20 @@ describe("home page test", () => {
         expect(buttonOne).toBeInTheDocument();
         expect(buttonTwo).toBeInTheDocument();
     });
-    it("button should redirect to new-request page", () => {
+    it("button should push to new-request page", () => {
         render(<App />);
         const buttonOne = screen.getByText("Cadastre uma nova inspeção");
         fireEvent.click(buttonOne);
         const title = screen.getByText("Faça uma nova solicitação");
+        expect(title).toBeInTheDocument();
+    });
+    it("button should push to solicitations page", () => {
+        render(<App/>);
+        const backButton = screen.getByText("voltar");
+        fireEvent.click(backButton);
+        const buttonTwo = screen.getByText("Consulte uma inspeção");
+        fireEvent.click(buttonTwo);
+        const title = screen.getByText("Solicitações");
         expect(title).toBeInTheDocument();
     });
 });
